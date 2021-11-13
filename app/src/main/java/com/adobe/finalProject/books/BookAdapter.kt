f249bd257f0dfb81a4ce561e0ace52ac1cfda337
Levon <levon@preezma.com>
2021-11-13 12:25:42 +0400

    Fixed Adapter Created method updateData

--- app/src/main/java/com/adobe/finalProject/books/BookAdapter.kt
+++ app/src/main/java/com/adobe/finalProject/books/BookAdapter.kt
@@ -1,76 +1,63 @@
 package com.adobe.finalProject.books
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.compose.runtime.toMutableStateList
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.adobe.finalProject.R
-import com.adobe.finalProject.stories.StoriesResponseModel
 import com.bumptech.glide.Glide
 
 class BookAdapter(
+    private val onClick: (url:String) -> Unit,
     private val context: Fragment,
     private var books: BooksResponseModel?
 ) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
     class BookViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val bookClickStory: LinearLayoutCompat = itemView.findViewById(R.id.book_story)
-        val sourceid: AppCompatTextView = itemView.findViewById(R.id.source_id)
-        val published_dateid: AppCompatTextView = itemView.findViewById(R.id.published_date_id)
-        val adx_keywords_id: AppCompatTextView = itemView.findViewById(R.id.adx_keywords_id)
-        val title: AppCompatTextView = itemView.findViewById(R.id.title_id)
-        val abstract: AppCompatTextView = itemView.findViewById(R.id.abstract_id)
-        val ImageUrl: AppCompatImageView = itemView.findViewById(R.id.image)
-
-//        fun setImageView1(image: BooksResponseModel.Result.Media.MediaMetadata, context: Context) {
-//
-////            Glide.with(context)
-////                .load(image.url)
-////                .centerCrop()
-////                .placeholder(R.drawable.ic_newspaper_outline)
-////                .into(ImageUrl)
-//        }
-
-
+        val imageView = itemView.findViewById<AppCompatImageView>(R.id.bookimage)
+        val titleTv = itemView.findViewById<AppCompatTextView>(R.id.bookName)
+        val author = itemView.findViewById<AppCompatTextView>(R.id.authorInfo)
+        val section = itemView.findViewById<AppCompatTextView>(R.id.section)
+        val publishDate = itemView.findViewById<AppCompatTextView>(R.id.published_date)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
         return BookViewHolder(
-
-            LayoutInflater.from(parent.context).inflate(R.layout.item_book, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_popular, parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-        holder.sourceid.text = books?.results?.get(position)?.source
-        holder.published_dateid.text = books?.results?.get(position)?.published_date
-        holder.adx_keywords_id.text = books?.results?.get(position)?.adx_keywords
-        holder.title.text = books?.results?.get(position)?.adx_keywords
-        holder.abstract.text = books?.results?.get(position)?.abstract
+        holder.titleTv.text = books?.results?.get(position)?.title
+        holder.publishDate.text = books?.results?.get(position)?.published_date
+        holder.author.text = books?.results?.get(position)?.byline
+        holder.section.text = books?.results?.get(position)?.section
+        holder.itemView.setOnClickListener {
+            onClick.invoke(books?.results?.get(position)?.url?:"")
+        }
+        if (books?.results?.get(position)?.media.isNullOrEmpty().not()) {
+            Glide
+                .with(context)
+                .load(books?.results?.get(position)?.media?.get(0)?.mediametadata?.get(2)?.url)
+                .centerCrop()
+                .placeholder(R.drawable.ic_newspaper_outline)
+                .into(holder.imageView)
+        }
 
-
-
-
-        Glide
-            .with(context)
-            .load(books?.results?.get(position)?.media?.get(position)?.mediametadata?.get(position))
-            .centerCrop()
-            .placeholder(R.drawable.ic_newspaper_outline)
-            .into(holder.ImageUrl)
-
+//        Log.i("ABC",
+//            books?.results?.get(position)?.media?.get(0)?.mediametadata?.get(0)?.url
+//                ?: ""
+//        )
     }
 
-    override fun getItemCount(): Int = books?.results?.size?:0
+    override fun getItemCount(): Int = books?.results?.size ?: 0
 
     fun setbook(items: BooksResponseModel?) {
 
         books = items
-        notifyItemRangeChanged(0, books?.results?.size?:0)
+        notifyItemRangeChanged(0, books?.results?.size ?: 0)
     }
 }
 
--- app/src/main/java/com/adobe/finalProject/books/BooksFragment.kt
+++ app/src/main/java/com/adobe/finalProject/books/BooksFragment.kt
@@ -6,13 +6,14 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.adobe.finalProject.R
+import com.adobe.finalProject.goToAnotherAppByUrl
 import com.adobe.finalProject.utils.Constants.API_KEY
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class BooksFragment : Fragment(R.layout.fragment_book) {
 
     private val booksViewModel: BooksViewModel by viewModel()
-    private val adapter =BookAdapter(this,BooksResponseModel())
+    private val adapter =BookAdapter(::goToArticle,this,BooksResponseModel())
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -39,6 +40,9 @@ class BooksFragment : Fragment(R.layout.fragment_book) {
         })
     }
 
+    private fun goToArticle(url:String){
+        goToAnotherAppByUrl(url)
+    }
 
 
 
